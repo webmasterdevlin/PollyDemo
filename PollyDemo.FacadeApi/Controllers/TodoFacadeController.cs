a4a9ffe2d3d8891c20907457cbd1f0f132abec13
Devlin Duldulao <webmasterdevlin@gmail.com>
2022-03-13 12:59:00 +0100

    add Ardalis.GuardClauses

--- PollyDemo.FacadeApi/Controllers/TodoFacadeController.cs
+++ PollyDemo.FacadeApi/Controllers/TodoFacadeController.cs
@@ -1,3 +1,4 @@
+using Ardalis.GuardClauses;
 using Microsoft.AspNetCore.Mvc;
 using PollyDemo.FacadeApi.Policies;
 
@@ -9,7 +10,7 @@ public class TodoFacadeController : ControllerBase
     {
     private readonly ClientPolicy _clientPolicy;
     private readonly IHttpClientFactory _clientFactory;
-
+    
     public TodoFacadeController(ClientPolicy clientPolicy, IHttpClientFactory clientFactory)
     {
         _clientPolicy = clientPolicy;
@@ -19,17 +20,19 @@ public class TodoFacadeController : ControllerBase
     [HttpGet("{id:int}")]
     public async Task<ActionResult> GetTodoById(int id)
     {
+        Guard.Against.OutOfRange(id, nameof(id), 1, 100, "Only 1 to 100 are accepted");
+        
         var client = _clientFactory.CreateClient();
         
         var response = await _clientPolicy.ExponentialHttpRetry.ExecuteAsync(()
             => client.GetAsync($"https://localhost:7211/api/todos/{id}"));
-
+        
         if(response.IsSuccessStatusCode)
         {
             Console.WriteLine("--> FacadeApi RECEIVED a SUCCESS");
             return Ok();
         }
-
+        
         Console.WriteLine("--> FacadeApi RECEIVED a FAILURE");
         return StatusCode(StatusCodes.Status500InternalServerError);
     }
--- PollyDemo.Todo/Controllers/TodoController.cs
+++ PollyDemo.Todo/Controllers/TodoController.cs
@@ -1,3 +1,4 @@
+using Ardalis.GuardClauses;
 using Microsoft.AspNetCore.Mvc;
 
 namespace PollyDemo.Todo.Controllers;
@@ -9,6 +10,8 @@ public class TodoController: ControllerBase
     [HttpGet("{id:int}")]
     public ActionResult GetTodoById(int id)
     {
+        Guard.Against.OutOfRange(id, nameof(id), 1, 100, "Only 1 to 100 are accepted");
+        
         var random = new Random();
         var randomNumber = random.Next(1, 101);
         if(randomNumber >= id )
@@ -16,7 +19,7 @@ public class TodoController: ControllerBase
             Console.WriteLine("--> TodoService Returned 500 ERROR");
             return StatusCode(StatusCodes.Status500InternalServerError);
         }
-
+        
         Console.WriteLine("--> TodoService Returned 200 OK");
         return Ok();
     }
