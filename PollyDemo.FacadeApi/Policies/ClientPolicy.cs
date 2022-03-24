4d2e7f4a3ba002b6167cb55d09a170734556d8cd
Devlin Duldulao <webmasterdevlin@gmail.com>
2022-03-24 10:46:23 +0100

    update

--- PollyDemo.FacadeApi/Policies/ClientPolicy.cs
+++ PollyDemo.FacadeApi/Policies/ClientPolicy.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Polly;
 using Polly.Retry;
 
@@ -5,9 +6,9 @@ namespace PollyDemo.FacadeApi.Policies;
 
 public class ClientPolicy
 {
-    public AsyncRetryPolicy<HttpResponseMessage> ImmediateHttpRetry { get;}
-    public AsyncRetryPolicy<HttpResponseMessage> LinearHttpRetry {get;}
-    public AsyncRetryPolicy<HttpResponseMessage> ExponentialHttpRetry {get;}
+    public AsyncRetryPolicy<HttpResponseMessage> ImmediateHttpRetry { get; }
+    public AsyncRetryPolicy<HttpResponseMessage> LinearHttpRetry { get; }
+    public AsyncRetryPolicy<HttpResponseMessage> ExponentialHttpRetry { get; }
 
     public ClientPolicy()
     {
@@ -19,8 +20,21 @@ public class ClientPolicy
                 res => !res.IsSuccessStatusCode)
             .WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(3));
 
+        // retries exponentially increase the waiting time up to a certain threshold
+        // not overwhelmed with requests hitting at the same time when it comes back up
         ExponentialHttpRetry = Policy.HandleResult<HttpResponseMessage>(
-                res => !res.IsSuccessStatusCode)
+                res =>  IsInKnownServerErrors(res.StatusCode))
             .WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));            
+    }
+
+    private bool IsInKnownServerErrors(HttpStatusCode returnedStatusCode)
+    {
+       return new List<HttpStatusCode>
+        {
+            HttpStatusCode.InternalServerError,
+            HttpStatusCode.BadGateway,
+            HttpStatusCode.ServiceUnavailable,
+            HttpStatusCode.GatewayTimeout
+        }.Contains(returnedStatusCode);
     }
 }
