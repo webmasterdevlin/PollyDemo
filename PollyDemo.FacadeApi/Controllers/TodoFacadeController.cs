9295f7bb791721b0319de9098ed9588be4411ae4
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-08-15 13:26:03 +0200

    update

--- PollyDemo.FacadeApi/Controllers/TodoFacadeController.cs
+++ PollyDemo.FacadeApi/Controllers/TodoFacadeController.cs
@@ -3,10 +3,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace PollyDemo.FacadeApi.Controllers;
 
+/// <summary>
+/// API Controller responsible for handling requests related to "Todo" items through a facade pattern.
+/// This controller interacts with a backend API and applies Polly policies such as retry and circuit breaker.
+/// </summary>
 [ApiController]
 [Route("api/facade/todos")]
 public class TodoFacadeController(IHttpClientFactory clientFactory) : ControllerBase
 {
+    /// <summary>
+    /// Retrieves a "Todo" item by its ID, interacting with a backend API.
+    /// This method checks the validity of the provided ID and uses Polly policies (retry and circuit breaker) 
+    /// to handle potential transient failures when calling the backend API.
+    /// </summary>
+    /// <param name="id">The ID of the "Todo" item to retrieve. Must be between 1 and 100.</param>
+    /// <returns>An <see cref="ActionResult"/> containing the "Todo" item if successful, or an error status if not.</returns>
     [HttpGet("{id:int}")]
     public async Task<ActionResult> GetTodoById(int id)
     {
--- PollyDemo.Todo/Controllers/TodoController.cs
+++ PollyDemo.Todo/Controllers/TodoController.cs
@@ -3,10 +3,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace PollyDemo.Todo.Controllers;
 
+/// <summary>
+/// API Controller responsible for handling requests related to "Todo" items.
+/// This controller simulates responses with either a success or a failure based on a random number generation.
+/// </summary>
 [ApiController]
 [Route("api/todos")]
 public class TodoController: ControllerBase
 {
+    /// <summary>
+    /// Retrieves a "Todo" item by its ID.
+    /// This method checks the validity of the provided ID and simulates a success or failure response
+    /// based on a random number comparison.
+    /// </summary>
+    /// <param name="id">The ID of the "Todo" item to retrieve. Must be between 1 and 100.</param>
+    /// <returns>An <see cref="ActionResult"/> containing the "Todo" item if successful, or an error status if not.</returns>
     [HttpGet("{id:int}")]
     public ActionResult GetTodoById(int id)
     {
